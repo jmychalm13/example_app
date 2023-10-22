3a943b302e365302fecb0d39f9fd89b0dc77b786
“Jessica <jmychalm13@gmail.com>
2023-10-22 10:41:20 -0500

    adds alternate code for fortune action

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,17 +1,22 @@
 class MyExamplesController < ApplicationController
   def random_fortune
-    fortune = ""
-    random_number = rand(3)
-    if random_number == 0
-      fortune = "You will have an awesome day!"
-    elsif random_number == 1
-      fortune = "You will be a coder soon!"
-    elsif random_number == 2
-      fortune = "You will be a coder very soon!"
-    end
+    # fortune = ""
+    # random_number = rand(3)
+    # if random_number == 0
+    #   fortune = "You will have an awesome day!"
+    # elsif random_number == 1
+    #   fortune = "You will be a coder soon!"
+    # elsif random_number == 2
+    #   fortune = "You will be a coder very soon!"
+    # end
 
+    fortunes = [
+      "You will become a coder soon!",
+      "You will find a better job soon",
+      "You are awesome at everything you do!"
+    ]
 
-    render json: {message: "#{fortune}"}
+    render json: {message: fortunes.sample}
   end
 
   def lotto_nums
